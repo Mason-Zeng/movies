60f9ee7bbc95f7e012fb50c14534d79fea9d9641
Mason-Zeng <masonzeng123@gmail.com>
2024-07-09 12:01:23 -0400

    connected to movie api

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+//f45c53e6
+
+const API_URL = 'http://www.ombdaoi.com?apikey=f45c53e6'
+
+const App = () => {
+    return(
+        <h1>App</h1>
+    );
+}
+
+export default App;
